5caf0966bcdc5994c853c56d235bb389361e0576
r3stl355 <r3stl355>
2021-12-24 15:05:34 +0000

    add parsing for image format

--- show-changing-images.py
+++ show-changing-images.py
@@ -1,13 +1,20 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC 
-# MAGIC ### Setup
+# MAGIC ### Using Databricks Platform Realtime Computer Vision
+
+# COMMAND ----------
+
+# MAGIC %pip install opencv-python
 
 # COMMAND ----------
 
 import os
 
-from pyspark.sql.types import *
+import cv2
+import numpy as np
+
+import pyspark.sql.types as T
 import pyspark.sql.functions as F
 
 # COMMAND ----------
@@ -18,13 +25,18 @@ SRC_FILE_PATH = os.path.join(SRC_DIR, SRC_FILE)
 
 DST_FILE_PATH = "dbfs:/FileStore/nul/result.jpg"
 
+dst_dir_local = os.path.split(DST_FILE_PATH)[0].replace("dbfs:", "/dbfs")
+if not os.path.exists(dst_dir_local):
+  os.makedirs(dst_dir_local, exists_ok=True)
+dst_file_path_local = DST_FILE_PATH.replace("dbfs:", "/dbfs")
+
 # COMMAND ----------
 
-def save_image_to(dst_path, simulate_slow=True):
-  def save_image(row):
+def save_image_to(dst_path, is_binary=True, simulate_slow=True):
+  def save_binary(row):
     import time
     
-#     dst = os.path.join(dir_path, os.path.split(row.path)[1])
+    # dst = os.path.join(dir_path, os.path.split(row.path)[1])
     dst = dst_path
     with open(dst, 'wb') as fout:
       fout.write(row.content)
@@ -32,25 +44,36 @@ def save_image_to(dst_path, simulate_slow=True):
     if simulate_slow:
       # Give some delay to simulate slow arrival of images
       time.sleep(0.1)
+
+  def save_image(row):
+    import time
+    import cv2
+    import numpy as np
+
+    img_arr = np.reshape(np.frombuffer(row.image.data, dtype=np.uint8), (row.image.height, row.image.width, -1))
+    cv2.imwrite(dst_path, img_arr)
     
-  return save_image
+    if simulate_slow:
+      # Give some delay to simulate slow arrival of images
+      time.sleep(0.1)
+    
+  return save_binary if is_binary else save_image
+
+def get_type(content):
+  return type(content)
+
+get_type_udf = spark.udf.register("get_type_udf", get_type, T.StringType())
 
 # COMMAND ----------
 
-# Read the file as binary as it preserves the original file content as bytes (loading it as `format('image')` seems to either base64 encode it or have it in CV2 format)
-
-df_images = spark.read.format("binaryFile").load(SRC_DIR).withColumn("length", F.length(F.col("content")))
+# Read the file as 'format("binaryFile")' because it preserves the original file content as bytes (loading it as `format('image')` seems to convert it in CV2 format)
+df_images = spark.read.format("binaryFile").load(SRC_DIR).withColumn("length", F.length(F.col("content"))).withColumn("type", get_type_udf("content"))
 df_images.printSchema()
 display(df_images)
 
 # COMMAND ----------
 
-dst_dir = os.path.split(DST_FILE_PATH)[0].replace("dbfs:", "/dbfs")
-if not os.path.exists(dst_dir):
-  os.makedirs(dst_dir, exists_ok=True)
-dst_file_path = DST_FILE_PATH.replace("dbfs:", "/dbfs")
-
-df_images.foreach(save_image_to(dst_file_path))
+df_images.foreach(save_image_to(dst_file_path_local))
 
 # COMMAND ----------
 
@@ -70,15 +93,43 @@ displayHTML(html)
 
 # COMMAND ----------
 
-
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Use `image` format
+# MAGIC 
+# MAGIC So what's stored in `image.data` if using `.format('image')`? Turns out it's a numpy/cv2 array in BGR format
 
 # COMMAND ----------
 
-
+# Read the file as `format('image')` - type of the content is the same here but length is different, maybe because it is in CV2 format
+images = (spark.read.format("image").load(SRC_DIR)
+  .withColumn("length", F.length(F.col("image.data")))
+  .withColumn("type", get_type_udf("image.data")).limit(2)
+  .withColumn("width", F.col("image.width"))
+  .withColumn("height", F.col("image.height")))
+images.printSchema()
+display(images)
 
 # COMMAND ----------
 
+import matplotlib.pyplot as plt
 
+image_row = images.collect()
+image_data = image_row[0].image.data
+img_arr = np.reshape(np.frombuffer(image_data, dtype=np.uint8), (441, 500, -1))
+img_arr = cv2.cvtColor(img_arr, cv2.COLOR_BGR2RGB)
+
+plt.imshow(img_arr)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC #### So we can achieve the same as above using
+
+# COMMAND ----------
+
+images.limit(1).foreach(save_image_to(dst_file_path_local, is_binary=False))
 
 # COMMAND ----------
 
